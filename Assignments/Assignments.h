1128a510dce7175c2e0e020ffb45baf63c310be1
Jesse <jessew7980@gmail.com>
2022-02-19 12:54:25 -0800

    Create Assignments

---
+++ Assignments/Assignments.h
@@ -0,0 +1,8 @@
+/*
+* Programmer: Jesse Watson
+* Current Class: CptS 122, Spring 2022; Lab Section 6
+* Assignment: CrimsonCode Hackathon 2022
+* Date: February 19, 2022
+* Description: This program takes a .csv of a student's past and upcoming assignments for the semester and allows them
+*			   to traverse the assignments by date, priority, class, upcoming, and past.
+*/
